e6e498c5ca347999e8f05810ac8d25761f1b3e21
Swarnab Garang <swarnabgarang@gmail.com>
2022-01-04 18:21:52 +0530

    public modules, enums & structs

--- crates-demo/restaurant/src/lib.rs
+++ crates-demo/restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
     
         fn seat_at_table() {}
     }
@@ -12,4 +12,45 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+use crate::front_of_house::hosting::add_to_waitlist;
+
+pub fn eat_at_restaurant() {
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    //'use' for path
+    add_to_waitlist();
+    add_to_waitlist();
+}
+
+mod back_of_house {
+
+    pub enum Appetizer {
+        Soup, 
+        Salad,
+    }
+    // public enum implies everything inside is public
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from('peaches'),
+            }
+        }
+    }
+}
+
+
+pub fn eat_at_restaurant_back() {
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;   
 }
